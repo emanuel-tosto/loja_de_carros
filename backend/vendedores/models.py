2084f829a3a855257856dc6c38df2f551a0ac8c7
Emanuel <emanueltosto@gmail.com>
2022-12-31 17:42:24 -0300

    criando o modelo de vendedores

--- backend/vendedores/models.py
+++ backend/vendedores/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Vendedor(models.Model):
+    nome = models.ChaField(max_length=50)
+    foto = models.ImageField(uplload_to='images')
+    descricao = models.TextField()
+    email = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
